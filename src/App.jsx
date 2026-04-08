a054a1f15d742dd58c5014cbb01bd045b07131a9
Oloyede Ola <israeloloyede002@gmail.com>
2026-04-08 14:57:39 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen text-5xl text-center items-center  bg-white font-bold text-blue-800 flex justify-center
+    '>Thank God for life of all of  us and people at home</div>
+  )
+}
+
+export default App 
